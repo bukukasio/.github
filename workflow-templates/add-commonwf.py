aaa5ff00af92b9ff4b4c8ea9e909ef2516a80219
jithesh <jithesh@keyvalue.systems>
2022-08-30 17:50:46 +0530

    add: common-wf script

--- workflow-templates/add-commonwf.py
+++ workflow-templates/add-commonwf.py
@@ -39,43 +39,88 @@ def copyWorkflows(repoPath,repoName):
         wfDirectoryPath = Path(WfDirectory)
         wfDirectoryPath.mkdir(parents=True, exist_ok=True)
         os.system("cp "+"./common-workflow.yml "+WfDirectory)
-        os.system("code "+repoPath)
+        # os.system("code "+repoPath)
         return True
-
 def pushChangesDefault(repoPath):
     try:
         repo = Repo(repoPath)
         repo.git.add(A=True)
         repo.config_writer().set_value("user", "name", "wfupdate").release()
-        repo.config_writer().set_value("user", "email", "wfupdate@keyvalue.systems").release()
-        repo.git.commit(m="Add common workflow file")
+        repo.config_writer().set_value("user", "email", "wfupdate@lummo.com").release()
+        repo.git.commit(m="add:common workflow file")
         repo.git.push("origin")
     except:
         print('Some error occured while pushing the code'+repoPath)
 
-def pushChangesMaster(repoPath,repoName): 
+def pushChangesMaster(repoPath,repoName,mainBranch): 
+    
     try:
         repo = Repo(repoPath)
-        repo.git.checkout("master")
-        copyWorkflows(repoPath,repoName)
+        repo.git.checkout(mainBranch)
+        pushStatus=copyWorkflows(repoPath,repoName)
         if pushStatus:
-            repo.git.add(A=True)
-            repo.config_writer().set_value("user", "name", "wfupdate").release()
-            repo.config_writer().set_value("user", "email", "wfupdate@keyvalue.systems").release()
-            repo.git.commit(m="Add common workflow file")
-            repo.git.push("origin")
+                os.system("code "+repoPath)
+                repo.git.add(A=True)
+                repo.config_writer().set_value("user", "name", "wfupdate").release()
+                repo.config_writer().set_value("user", "email", "wfupdate@lummo.com").release()
+                repo.git.commit(m="add: common workflow file")
+                repo.git.push("origin")
     except:
         print('Some error occured while pushing the code in master '+repoName)
 
+def findBranches(repoPath,repoName):
+    try:
+        repo = Repo(repoPath)
+        defaultBranch = repo.active_branch.name
+        print ("Default Branch for "+repoName+": "+defaultBranch)
+        mainBranch=defaultBranch
+        remote_refs = repo.remote().refs
+        for refs in remote_refs:
+            # print (refs.name)
+            if refs.name == "origin/master" or refs.name == "origin/main":
+                mainBranch=refs.name
+                if mainBranch == "origin/master":
+                    mainBranch="master"
+                else:
+                    mainBranch="main"
+        print ("Main Branch for "+repoName+": "+mainBranch)
+        return defaultBranch,mainBranch,True
+    except:
+        print("Unable to Read from "+repoName)
+        return "master","main",False
+
 if __name__ == "__main__":
-
     repos=listRepos()
-
+    print(len(repos))
+    # repoPath,repoName=cloneRepo("singlerepo")
+    # defaultBranch,mainBranch,branchStatus=findBranches(repoPath,repoName)
+    # if branchStatus:
+    #     if defaultBranch==mainBranch:
+    #         pushChangesMaster(repoPath,repoName,mainBranch)
+    #     else:
+    #         pushStatus=copyWorkflows(repoPath,repoName)
+    #         print(pushStatus)
+    #         if pushStatus:
+    #             pushChangesDefault(repoPath)
+    #         pushChangesMaster(repoPath,repoName,mainBranch)
+    # else:
+    #     print("unable to get branch details")
+    count=0
     for repo in repos:
-        repoName=str(repo)
-        repoPath,repoName=cloneRepo(repoName)
-        pushStatus=copyWorkflows(repoPath,repoName)
-        if pushStatus:
-            pushChangesDefault(repoPath)
-            pushChangesMaster(repoPath,repoName)
-
+        count=count+1
+        print (count)
+        if (count!=113):
+            repoName=str(repo)
+            print(repoName)
+            repoPath,repoName=cloneRepo(repoName)
+            defaultBranch,mainBranch,branchStatus=findBranches(repoPath,repoName)
+            if branchStatus:
+                if defaultBranch==mainBranch:
+                    pushChangesMaster(repoPath,repoName,mainBranch)
+                else:
+                    pushStatus=copyWorkflows(repoPath,repoName)
+                    if pushStatus:
+                        pushChangesDefault(repoPath)
+                    pushChangesMaster(repoPath,repoName,mainBranch)
+            else:
+                print("unable to get branch details")
